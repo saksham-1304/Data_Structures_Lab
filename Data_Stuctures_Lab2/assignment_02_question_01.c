541cd6e8f41e46b2594864421b6a952042b0484f
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2024-08-06 23:22:18 +0530

    Data Structures Lab2

---
+++ Data_Stuctures_Lab2/assignment_02_question_01.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+void findUnion(int arr1[], int size1, int arr2[], int size2)
+{
+    int unionArr[size1 + size2];
+    int unionSize = 0;
+    for (int i = 0; i < size1; i++)
+    {
+        unionArr[unionSize++] = arr1[i];
+    }
+    for (int i = 0; i < size2; i++)
+    {
+        int found = 0;
+        for (int j = 0; j < size1; j++)
+        {
+            if (arr2[i] == arr1[j])
+            {
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+        {
+            unionArr[unionSize++] = arr2[i];
+        }
+    }
+    printf("Union of arrays: ");
+    for (int i = 0; i < unionSize; i++)
+    {
+        printf("%d ", unionArr[i]);
+    }
+    printf("\n");
+}
+void findIntersection(int arr1[], int size1, int arr2[], int size2)
+{
+    printf("Intersection of arrays: ");
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            if (arr1[i] == arr2[j])
+            {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+    printf("\n");
+}
+int main()
+{
+    int size1, size2;
+    printf("Enter the size of the first array: ");
+    scanf("%d", &size1);
+    int arr1[size1];
+    printf("Enter the elements of the first array:\n");
+    for (int i = 0; i < size1; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+    printf("Enter the size of the second array: ");
+    scanf("%d", &size2);
+    int arr2[size2];
+    printf("Enter the elements of the second array:\n");
+    for (int i = 0; i < size2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+    findUnion(arr1, size1, arr2, size2);
+    findIntersection(arr1, size1, arr2, size2);
+    return 0;
+}
---
+++ Data_Stuctures_Lab2/assignment_02_question_02.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+#define MAX_ROWS 100
+#define MAX_COLS 100
+
+void multiplyMatrices(int mat1[][MAX_COLS], int rows1, int cols1, int mat2[][MAX_COLS], int rows2, int cols2, int result[][MAX_COLS])
+{
+    if (cols1 != rows2)
+    {
+        printf("Matrix multiplication is not possible.\n");
+        return;
+    }
+
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++)
+            {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+void displayMatrix(int mat[][MAX_COLS], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int mat1[MAX_ROWS][MAX_COLS], mat2[MAX_ROWS][MAX_COLS], result[MAX_ROWS][MAX_COLS];
+    int rows1, cols1, rows2, cols2;
+
+    printf("Enter the number of rows and columns for the first matrix: ");
+    scanf("%d %d", &rows1, &cols1);
+
+    printf("Enter the elements of the first matrix:\n");
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols1; j++)
+        {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    printf("Enter the number of rows and columns for the second matrix: ");
+    scanf("%d %d", &rows2, &cols2);
+
+    printf("Enter the elements of the second matrix:\n");
+    for (int i = 0; i < rows2; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    if (cols1 != rows2)
+    {
+        printf("Matrix multiplication is not possible.\n");
+        return 1;
+    }
+    multiplyMatrices(mat1, rows1, cols1, mat2, rows2, cols2, result);
+    printf("Resultant matrix after multiplication:\n");
+    displayMatrix(result, rows1, cols2);
+    return 0;
+}
